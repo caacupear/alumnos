e9caae211da4872388c15011590a9dc95bc167be
Sebastian Claros <sclaros@gmail.com>
2023-12-04 20:46:18 -0300

    pages

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+export default {
+    // ...
+    url: 'https://caacupe.github.io', // Your website URL
+    baseUrl: '/alumnos',
+    projectName: 'caacupe.github.io',
+    organizationName: 'caacupe',
+    trailingSlash: false,
+    // ...
+  };
