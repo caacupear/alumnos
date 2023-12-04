e68bbdd6585d9083cc11c50df5d130a89425da8c
Sebastian Claros <sclaros@gmail.com>
2023-12-04 15:54:57 -0300

    doc scripts

---
+++ scripts/doc/helpers/class.js
@@ -0,0 +1,18 @@
+
+async function prompt(config) {    
+
+}
+
+function help() {
+    console.log('Puede llamarse con el api name de un objeto o varios, de la siguiente forma:');
+}
+
+async function execute( clases) {
+
+}
+
+module.exports = {
+    prompt,
+    help,
+    execute
+};
