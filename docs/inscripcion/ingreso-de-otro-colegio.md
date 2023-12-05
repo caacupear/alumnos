a4b1c8029f9de98f8e16253e32cb81aa8d9588f1
Sebastian Claros <sclaros@gmail.com>
2023-12-05 10:27:08 -0300

    docusaurus

--- docs/inscripcion/ingreso-de-otro-colegio.md
+++ docs/inscripcion/ingreso-de-otro-colegio.md
@@ -1,10 +1,15 @@
+---
+
+---
+
 # Ingreso de otro colegio
 
 ## Contexto
-* Cuando: 
-* Precondición: Alumno que quiere ingresar desde otro colegio
-* Postcondición: Alumno quede en estado Inscripto y con las revalidación de materias
-* Actores: Secretaria
+
+- Cuando:
+- Precondición: Alumno que quiere ingresar desde otro colegio
+- Postcondición: Alumno quede en estado Inscripto y con las revalidación de materias
+- Actores: Secretaria
 
 ## Flujo Principal
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,107 @@
+// const lightCodeTheme = require('prism-react-renderer/themes/github');
+// const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
 export default {
-    title: 'Caacupe',
-    url: 'https://caacupe.github.io', // Your website URL
-    baseUrl: '/alumnos',
-    projectName: 'caacupe.github.io',
-    organizationName: 'caacupe',
-    trailingSlash: false,
-  };
+  title: "Caacupe",
+  url: "https://caacupe.github.io", // Your website URL
+  baseUrl: "/alumnos",
+  projectName: "caacupe.github.io",
+  organizationName: "caacupe",
+  trailingSlash: false,
+  onBrokenLinks: "log",
+  onBrokenMarkdownLinks: "warn",
+
+  plugins: [["drawio", {}]],
+
+  markdown: {
+    mermaid: true
+  },
+
+  themes: ["@docusaurus/theme-mermaid"],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es"]
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/", // Serve the docs at the site's root
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/caacupear/alumnos/tree/main"
+        },
+        blog: false
+        // blog: {
+        //   showReadingTime: true,
+        //   readingTime: ({content, frontMatter, defaultReadingTime}) =>
+        //     defaultReadingTime({content, options: {wordsPerMinute: 300}}),
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/caacupear/alumnos/tree/main',
+        // },
+      })
+    ]
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Caacupe",
+        logo: {
+          alt: "Caacupe",
+          src: "img/caacupe.png"
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "inscripcion",
+            position: "left",
+            label: "Inscripcion"
+          },
+          // {
+          //   to: 'blog',
+          //   label: 'Blog',
+          //   position: 'right'
+          // },
+          {
+            type: "docSidebar",
+            sidebarId: "diccionario",
+            position: "right",
+            label: "Diccionarios"
+          }
+        ]
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Apis",
+            items: [
+              // {
+              //   label: 'Portabilidad',
+              //   to: '/docs/intro',
+              // },
+            ]
+          },
+          {
+            title: "Arquitectura",
+            items: []
+          },
+          {
+            title: "Standards",
+            items: []
+          }
+        ]
+      }
+      // prism: {
+      //   theme: lightCodeTheme,
+      //   darkTheme: darkCodeTheme,
+      // },
+    })
+};
